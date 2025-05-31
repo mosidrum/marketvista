d884b9c5be3114b3e36f6b1fa9e29bc9d18e422c
mosidrum <mosiokanga@gmail.com>
2025-05-31 03:27:49 +0100

    increase item from product list

--- src/app/components/button/ProductButton.tsx
+++ src/app/components/button/ProductButton.tsx
@@ -1,17 +1,23 @@
 "use client";
-import { addToCart } from "@/app/redux";
+import { addToCart, removeItemFromCart } from "@/app/redux";
 import { AlertType, ProductionDataType } from "@/app/types";
 import { showAlert } from "@/app/utils";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { twMerge } from "tailwind-merge";
 
+export enum ButtonAction {
+  ADD = "Add",
+  REMOVE = "Remove",
+}
+
 interface Props {
   item: ProductionDataType;
   className?: string;
+  type: ButtonAction;
 }
 
-export const AddToCartButton = ({ item, className }: Props) => {
+export const ProductButton = ({ item, className, type }: Props) => {
   const dispatch = useDispatch();
 
   const handleAddToCart = (item: ProductionDataType) => {
@@ -22,15 +28,25 @@ export const AddToCartButton = ({ item, className }: Props) => {
     );
   };
 
+  const removeFromCart = (item: ProductionDataType) => {
+    dispatch(removeItemFromCart(item?._id));
+    showAlert(
+      `${item.title.substring(0, 12)} removed from cart`,
+      AlertType.SUCCESS
+    );
+  };
+
   return (
     <button
-      onClick={() => handleAddToCart(item)}
+      onClick={() =>
+        type === ButtonAction.ADD ? handleAddToCart(item) : removeFromCart(item)
+      }
       className={twMerge(
         `bg-accent text-white w-full py-2 border border-px border-accent hover:bg-darkOrange hover:border-darkOrange hoverEfffect font-semibold tracking-wide flex items-center justify-center gap-1`,
         className
       )}
     >
-      Add to cart
+      {type === ButtonAction.ADD ? "Add to cart" : "Remove from cart"}
     </button>
   );
 };
--- src/app/components/products/ProductCard.tsx
+++ src/app/components/products/ProductCard.tsx
@@ -1,15 +1,24 @@
-import { ProductionDataType } from "@/app/types";
+"use client";
+import { ProductionDataType, StoreState } from "@/app/types";
 import { urlFor } from "@/sanity/lib/image";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { MdStar } from "react-icons/md";
 import { FormattedPrice } from "../FormattedPrice";
-import { AddToCartButton } from "../button";
+import { useDispatch, useSelector } from "react-redux";
+import { decreaseQuantity, increaseQuantity } from "@/app/redux";
+import { HiMinus, HiPlus } from "react-icons/hi";
+import { ButtonAction, ProductButton } from "../button";
 
 export const ProductCard = ({ item }: { item: ProductionDataType }) => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state: StoreState) => state?.marketVista);
+  const isExist = cart.find((cartItem) => cartItem._id === item._id);
+  console.log({ isExist });
+
   return (
-    <div className="border border-px border-lightText/40 rounded-md relative group overflow-hidden">
+    <div className="border border-px border-lightText/40 rounded-md relative group overflow-hidden flex flex-col">
       <div className="overflow-hidden">
         <Link href={`/product/${item?.slug.current}`}>
           <Image
@@ -22,7 +31,8 @@ export const ProductCard = ({ item }: { item: ProductionDataType }) => {
           />
         </Link>
       </div>
-      <div className="px-6 flex flex-col items-center gap-2">
+
+      <div className="px-6 flex flex-col items-center gap-2 flex-grow">
         <div className="text-base text-lightText flex items-center">
           {Array?.from({ length: 5 }).map((_, index) => {
             const filled = index + 1 <= Math.floor(item.ratings);
@@ -52,7 +62,36 @@ export const ProductCard = ({ item }: { item: ProductionDataType }) => {
           <FormattedPrice amount={item.price} className="text-darkOrange" />
         </div>
       </div>
-      <AddToCartButton item={item} />
+
+      <div className="h-8 flex items-center justify-center px-2 mb-2">
+        {isExist && (
+          <div className="flex items-center justify-center space-x-5 md:gap-3">
+            <div className="w-4 md:w-6 flex justify-center">
+              {isExist?.quantity > 1 && (
+                <div className="border border-lightOrange p-2 rounded-lg text-lightOrange">
+                  <HiMinus
+                    className="hover:cursor-pointer"
+                    onClick={() => dispatch(decreaseQuantity(item._id))}
+                  />
+                </div>
+              )}
+            </div>
+            <p className="font-bold">{isExist?.quantity}</p>
+            <div className="border border-lightOrange p-2 rounded-lg text-lightOrange">
+              <HiPlus
+                className="hover:cursor-pointer"
+                onClick={() => dispatch(increaseQuantity(item._id))}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+
+      {isExist ? (
+        <ProductButton type={ButtonAction.REMOVE} item={item} />
+      ) : (
+        <ProductButton type={ButtonAction.ADD} item={item} />
+      )}
     </div>
   );
 };
