ab672d9ea815bc2b3df5b48a3b8f9e82f739ac51
mosidrum <mosiokanga@gmail.com>
2025-07-11 12:22:00 +0100

    success payment page

--- src/app/(user)/success/page.tsx
+++ src/app/(user)/success/page.tsx
@@ -1,12 +1,12 @@
 import { Container } from "@/app";
 
 interface SuccessPageProps {
-    searchParams: {
-        session_id?: string;
-    }
+  searchParams: {
+    session_id?: string;
+  };
 }
 
 export default function SuccessPage({ searchParams }: SuccessPageProps) {
-    console.log(searchParams)
-    return <Container>Success page</Container>
+  console.log(searchParams?.session_id);
+  return <Container>Success page</Container>;
 }
